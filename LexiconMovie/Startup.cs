846424ba19d83bd3b8bca5012d92f9e6b797cab0
Jansan <jansan-26@hotmail.com>
2020-10-09 12:49:53 +0200

    MoviesController

--- LexiconMovie/Startup.cs
+++ LexiconMovie/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using LexiconMovie.Data;
 
 namespace LexiconMovie
 {
@@ -24,6 +26,9 @@ namespace LexiconMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<LexiconMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("LexiconMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
