a7a2f224934ee7e027e5705193bb8be5fe7087f6
Jansan <jansan-26@hotmail.com>
2020-10-09 12:29:09 +0200

    Movie Model class and enum Genre added

---
+++ LexiconMovie/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconMovie.Models
+{
+    public class Movie
+    {
+        //Id
+        public int Id { get; set; }
+
+        //Title
+        [Required]
+        [StringLength(30)]
+        public string  Title { get; set; }
+
+        //ReleaseDate
+        [DataType(DataType.Date)]
+        [Display(Name = "Release date")]
+        public DateTime ReleaseDate { get; set; }
+
+        //Genre
+        [Required]
+        public Genre Genre { get; set; }
+
+        //Rating
+        [Range(1, 5)]
+        public float Rating { get; set; }
+    }
+}
